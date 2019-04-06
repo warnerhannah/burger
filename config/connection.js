aa193557343e475e4ce392b9c4dd11d1ded3716c
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-05 20:10:31 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1Hannahhaw",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection.connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1Hannahhaw",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(err => {
   if (err) {
